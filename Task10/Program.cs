4b124ec78223a7b77f64860fd6a4c582efbbf61a
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-27 08:34:43 +0300

    Finished Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,23 +5,25 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите целое положительное трехзначное число: "); //ввод данных
+Console.Write("Введите трехзначное число: ");                     //ввод данных
 int number = Convert.ToInt32(Console.ReadLine());                 //переменные
 
 int showSecondDigit = ShowSecondDigit(number);
 
-if (number > 100 && number < 1000)                               // условие
-{
-    Console.WriteLine($"Вторая цифра числа {showSecondDigit}");
-}
+if (number < 100 && number > -100) 
+Console.WriteLine ("Вы ввели не трехзначное число.");
+
+else if (number > 999 || number < - 999)
+Console.WriteLine ("Вы ввели не трехзначное число.");
+
 else
-    Console.WriteLine("Некорректный ввод");
-    
+Console.WriteLine($"Вторая цифра числа {showSecondDigit}");
+
 
 int ShowSecondDigit(int num)                                   // метод
 {
-    int remainder = number % 100; // remainder - остаток
-    return remainder / 10 ;
+    int remainder = (num % 100 ) / 10;  // remainder - остаток
+    return remainder = remainder < 0 ? remainder * (-1) : remainder ;
 }
 
 
