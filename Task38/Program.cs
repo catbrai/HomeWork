c76b1210de3ef8ce5792758a5effdfbf3b4112a8
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-06-10 14:30:19 +0300

    correct round Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,7 +2,7 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
-double[] CreateArrayRndDouble(int size, int min, int max, int round = 2)
+double[] CreateArrayRndDouble(int size, int min, int max, int round = 3)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
@@ -45,10 +45,10 @@ for (int i = 0; i < arr.Length; i++)
 return min1;   
 }
 
-double MaxMinDiff (double max1, double min1) 
+double MaxMinDiff (double max1, double min1, int round = 2) 
 {
 double num = max1 - min1;
-num = Math.Round(num, 2); // округление результата до двух знаков после запятой
+num = Math.Round(num, round); // округление результата до двух знаков после запятой
 return num;
 }
 
